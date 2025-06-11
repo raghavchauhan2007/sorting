af1bccc03ecbd3001e0b3cc012bc56542bd77caa
Raghav Chauhan <chauhan.raghav2020@gmail.com>
2025-06-11 18:59:08 +0530

    insertion sort

--- sorting.java
+++ sorting.java
@@ -8,20 +8,16 @@ public class sorting {
     }
 
     public static void main(String[] args) {
-        int arr[] = {7,8,3,1,2,10};
-        //selection sort
-        for(int i=0; i<arr.length-1; i++){
-            int smallest = i;
-            for(int j=i+1; j<arr.length; j++){
-                if(arr[smallest]>arr[j]){
-                    smallest=j;
-                }
+        int arr[] = {7,8,3,1,2,10,-1};
+        //insertion sort
+        for(int i=1; i<arr.length; i++){
+            int current=arr[i];
+            int j=i-1;
+            while(j>=0 && current<arr[j]){
+                arr[j+1]=arr[j];
+                j--;
             }
-            int temp=arr[smallest];
-            arr[smallest]=arr[i];
-            arr[i]=temp;
-
-
+            arr[j+1]=current;
         }
         printArray(arr);
     }
