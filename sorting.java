ae57f2a53d89594efce51913e7df0507f58b9f7d
Raghav Chauhan <chauhan.raghav2020@gmail.com>
2025-07-22 14:14:34 +0530

    mergeSort

--- sorting.java
+++ sorting.java
@@ -1,24 +1,54 @@
 public class sorting {
 
-    public static void printArray(int arr[]){
-        for(int a=0; a<arr.length; a++){
-            System.out.print(arr[a]+" ");
+    public static void conquer(int si, int mid, int ei, int[] arr) {
+        int[] merged = new int [ei-si+1];
+        int idx1 = si;//array1
+        int idx2 = mid+1;//array2
+        int x = 0;//mergedTracker
 
+        while(idx1 <= mid && idx2 <= ei) {
+            if(arr[idx1] <= arr[idx2]) {
+                merged[x++] = arr[idx1++];
+            }
+            else {
+                merged[x++] = arr[idx2++];
+            }
         }
+
+        while(idx1 <= mid) {
+            merged[x++] = arr[idx1++];
+        }
+
+        while(idx2 <= ei) {
+            merged[x++] = arr[idx2++];
+        }
+
+        for(int i=0, j=si; i<merged.length; i++, j++) {
+            arr[j] = merged[i];
+        }
+
+
+    }
+
+    public static void divide(int si, int ei, int [] arr) {
+
+        if(si>=ei) {
+            return;
+        }
+
+        int mid = si + (ei-si)/2;
+        divide(si, mid, arr);
+        divide(mid+1, ei, arr);
+        conquer(si, mid, ei, arr);
     }
 
     public static void main(String[] args) {
-        int arr[] = {7,8,3,1,2,10,-1};
-        //insertion sort
-        for(int i=1; i<arr.length; i++){
-            int current=arr[i];
-            int j=i-1;
-            while(j>=0 && current<arr[j]){
-                arr[j+1]=arr[j];
-                j--;
-            }
-            arr[j+1]=current;
+        int[] arr = {6, 3, 9, 5, 2, 8};
+        int n = arr.length;
+
+        divide(0, n-1, arr);
+        for(int i=0; i<n; i++) {
+            System.out.println(arr[i]+" ");
         }
-        printArray(arr);
     }
 }
