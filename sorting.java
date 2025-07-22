301e9cb49dd2fcb7bd1aa26bbd97543ac578cea5
Raghav Chauhan <chauhan.raghav2020@gmail.com>
2025-07-22 16:50:32 +0530

    quickSort

--- sorting.java
+++ sorting.java
@@ -1,53 +1,41 @@
 public class sorting {
+    public static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low-1;
 
-    public static void conquer(int si, int mid, int ei, int[] arr) {
-        int[] merged = new int [ei-si+1];
-        int idx1 = si;//array1
-        int idx2 = mid+1;//array2
-        int x = 0;//mergedTracker
-
-        while(idx1 <= mid && idx2 <= ei) {
-            if(arr[idx1] <= arr[idx2]) {
-                merged[x++] = arr[idx1++];
-            }
-            else {
-                merged[x++] = arr[idx2++];
+        for(int j=low; j<high; j++) {
+            if(arr[j] < pivot) {
+                i++;
+                //swap
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
             }
         }
-
-        while(idx1 <= mid) {
-            merged[x++] = arr[idx1++];
-        }
-
-        while(idx2 <= ei) {
-            merged[x++] = arr[idx2++];
-        }
-
-        for(int i=0, j=si; i<merged.length; i++, j++) {
-            arr[j] = merged[i];
-        }
-
-
+        i++;
+        //swap
+        int temp = arr[i];
+        arr[i] = arr[high];
+        arr[high] = temp;
+        return i; 
     }
 
-    public static void divide(int si, int ei, int [] arr) {
+    public static void quickSort(int[] arr, int low, int high) {
+        if(low<high) {
+            int pidx = partition(arr, low, high);
 
-        if(si>=ei) {
-            return;
+            quickSort(arr, low, pidx-1);
+            quickSort(arr, pidx+1, high);
         }
-
-        int mid = si + (ei-si)/2;
-        divide(si, mid, arr);
-        divide(mid+1, ei, arr);
-        conquer(si, mid, ei, arr);
     }
 
     public static void main(String[] args) {
-        int[] arr = {6, 3, 9, 5, 2, 8};
+        int[] arr = {6,3,9,5,2,8};
         int n = arr.length;
 
-        divide(0, n-1, arr);
-        for(int i=0; i<n; i++) {
+        quickSort(arr, 0, n-1);
+
+        for(int i=0; i<arr.length; i++) {
             System.out.println(arr[i]+" ");
         }
     }
